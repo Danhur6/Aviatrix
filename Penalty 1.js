9b4526903a59d8b8c631370c3ad323db35ec066c
Danhur6 <pipe0625@hotmail.com>
2025-08-14 19:00:30 -0500

    Add files via upload

---
+++ Penalty 1.js
@@ -0,0 +1,12 @@
+<!doctype html><html lang="en"><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,shrink-to-fit=no"/><meta name="HandheldFriendly" content="true"><meta name="theme-color" content="#000000"/><meta name="description" content="Mine Game"/><link rel="apple-touch-icon" href="/favicon.svg"/><link rel="icon" type="image/svg+xml" href="/favicon.svg"/><title>Mine game</title><style>html,
+    body {
+      position: relative;
+      height: 100%;
+      width: 100%;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background: #000000;
+      color: #ffffff;
+      touch-action: none;
+    }</style><script defer="defer" src="game.aa278bee974f0b89ca51.js"></script><script defer="defer" src="game.22c2f6395695cac321fa.js"></script><link href="main.976eb694f19abd40f3b9.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="app"><canvas id="canvas"></canvas><div id="interface"></div></div><script>document.addEventListener('gesturestart', (e) => e.preventDefault());</script></body></html>
